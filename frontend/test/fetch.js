972c569f9c31ca5840134dbbff6baa9042e4315d
Thien DAO <thien1997nus@gmail.com>
2024-03-14 19:26:39 +0700

    feat: update html table per interval pure js

--- frontend/test/fetch.js
+++ frontend/test/fetch.js
@@ -1,6 +1,54 @@
-const h3text = document.querySelector('div h3');
-h3text.style.color = 'Blue'
+const getData = () => {
+    return fetch('client-data.json')
+        .then( response => {
+            return response.json();
+        });
+};
+const updateDataTable = (data) =>{
+    console.info("Updating items table ...");
+    const tableBody = document.querySelector('#client-table tbody');
+        tableBody.innerHTML="";
 
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.contact}</td>
+                <td>${item.phonenum}</td>
+                <td>${item.location}</td>
+                <td>${item.status}</td>
+            `;
+            tableBody.appendChild(row);
+        });
+}
+
+getData().then( items => {
+    updateDataTable(items);
+});
+
+/*
+datafunction: method to get data
+oncomplete: execute when data is back
+*/
+
+const autoRefresh = ( {dataFunction, onComplete, interval = 30000}) => {
+    const execute = () => {
+        dataFunction().then( data => {
+            onComplete(data);
+            setTimeout(execute,interval);
+        })
+    };
+
+    execute();
+};
+
+autoRefresh({
+    dataFunction: getData,
+    onComplete: updateDataTable,
+    interval: 2000
+});
+
+/*
 fetch('client-data.json')
     .then( response => response.json())
     .then( data => {
@@ -19,3 +67,4 @@ fetch('client-data.json')
         });
     })
     .catch( error => console.error('Error fetching data', error));
+*/
