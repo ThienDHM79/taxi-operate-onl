f68f11c4cf7357816a6c43ec71965b419c8a9080
Thien DAO <thien1997nus@gmail.com>
2024-04-06 17:49:17 +0700

    feat: create trip front sucess write db

--- frontend/controllers/clientController.js
+++ frontend/controllers/clientController.js
@@ -1,17 +1,44 @@
 'use strict';
 let controller = {};
 const axios = require('axios');
-const baseUrl = 'http://localhost:7000/v1/clients';
+const { stringify } = require('nodemon/lib/utils');
+const baseUrl = 'http://localhost:7000';
 const getDataAxios = async (apiUrl) => {
     const response =  await axios.get(apiUrl);
     return response.data;
  
 };
+class DTO{
+    customername;
+    customerphone;
+    taxitype;
+    constructor(data){
+        this.customername = data.customername;
+        this.customerphone = data.customerphone;
+        this.taxitype = data.taxitype;
+    
+    }
+}
+
+
+const postDataAxios = async(data, apiUrl) => {
+    const json = JSON.stringify(data);
+    console.log(json);
+    const res = await axios.post(apiUrl, json, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    return res.data.data;
+}
 
 controller.getData = async (req, res, next) => {
     //const clientsData = await getDataAxios(baseUrl);
+
+    //update to load data of trip. current data of client (NOT UPDATE AFTER ADD TRIP)
     try {
-        const ClientsData = await getDataAxios(baseUrl);
+        const ClientsData = await getDataAxios(baseUrl+'/v1/clients');
+        console.log(ClientsData);
         res.locals.clients = ClientsData;
         res.locals.noti_message = 'Successful';
     } catch (error){
@@ -22,6 +49,19 @@ controller.getData = async (req, res, next) => {
 }
 controller.show = async (req, res) => {
     res.render('client');
+}
+
+controller.create= async( req, res, next) => {
+    try{
+        const createTripData = new DTO(req.body);
+        const createTrip = await postDataAxios(createTripData, baseUrl + '/v1/Booking', );
+        await console.log(`create success ${createTrip}`);
+
+    } catch (error){
+        res.locals.noti_message = error.message;
+        console.log(error.message);
+    }
+    next();
 }
 
 module.exports = controller;
--- frontend/routes/clientRouter.js
+++ frontend/routes/clientRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require('../controllers/clientController');
 
 router.get('/',controller.getData, controller.show);
+router.post('/',controller.create, controller.getData, controller.show);
 
 module.exports = router;
